b06459c465c7f1d8eb00a570a312469c8be7a9d2
PolamW <pwong140@my.centennialcollege.ca>
2023-01-29 20:39:41 -0500

    added 3 clouds to the scene

--- MAPD724-W2023-ICE-2A/GameScene.swift
+++ MAPD724-W2023-ICE-2A/GameScene.swift
@@ -13,7 +13,7 @@ class GameScene: SKScene {
     var ocean2: Ocean?
     var player: Player?
     var island: Island?
-    var cloud: Cloud?
+    var clouds: [Cloud] = []
 
     
     override func sceneDidLoad()
@@ -43,9 +43,13 @@ class GameScene: SKScene {
         island = Island()
         addChild(island!)
         
-        //add the cloud to the scene
-        cloud = Cloud()
-        addChild(cloud!)
+        //add three cloud to the scene
+        for _ in 0...2
+        {
+           let cloud = Cloud()
+            clouds.append(cloud)
+            addChild(cloud)
+        }
     }
     
     func touchDown(atPoint pos : CGPoint)
@@ -85,6 +89,11 @@ class GameScene: SKScene {
         ocean2?.Update()
         player?.Update()
         island?.Update()
-        cloud?.Update()
+        
+        //update each cloud in the clouds array
+        for cloud in clouds
+        {
+            cloud.Update()
+        }
     }
 }
